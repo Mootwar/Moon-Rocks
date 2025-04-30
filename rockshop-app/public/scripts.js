4f162d8df6d88c3cff08d1837bfb908196b48adb
QuadeCSquared <quadeccunningham@gmail.com>
2025-04-29 19:55:42 -0600

    Added Weight Column

--- rockshop-app/public/scripts.js
+++ rockshop-app/public/scripts.js
@@ -23,6 +23,7 @@ function displayMinerals(minerals) {
       <td>${mineral.name}</td>
       <td>$${parseFloat(mineral.price).toFixed(2)}</td>
       <td>${mineral.amount}</td>
+      <td>${mineral.weight}</td>
       <td><img src="/uploads/${mineral.photo}" alt="${mineral.name}" width="100"></td>
     `;
 
